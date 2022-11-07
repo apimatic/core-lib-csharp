db746169a77fd397ba22146a4c04f6a0838edf8a
Asad Ali <asad.ali@apimatic.io>
2022-11-07 17:28:44 +0500

    updated ICompatibilityFactory with CreateApiException function

--- APIMatic.Core/Utilities/ICompatibilityFactory.cs
+++ APIMatic.Core/Utilities/ICompatibilityFactory.cs
@@ -5,11 +5,13 @@ using APIMatic.Core.Types.Sdk;
 
 namespace APIMatic.Core.Utilities
 {
-    public interface ICompatibilityFactory<Request, Response, Context>
+    public interface ICompatibilityFactory<Request, Response, Context, ApiException>
         where Request : CoreRequest
         where Response : CoreResponse
         where Context: CoreContext<Request, Response>
+        where ApiException : CoreApiException<Request, Response, Context>
     {
+        ApiException CreateApiException(string reason, CoreContext<Request, Response> context);
         Request CreateHttpRequest(CoreRequest request);
         Response CreateHttpResponse(CoreResponse response);
         Context CreateHttpContext(CoreRequest request, CoreResponse response);
