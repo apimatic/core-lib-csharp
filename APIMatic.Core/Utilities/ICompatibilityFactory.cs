f020e5909e62d35ea4bc40b03da2e608c74ae01a
Asad Ali <asad.ali@apimatic.io>
2022-12-22 19:19:39 +0500

    Added summary in ICompatibilityFactory interface

--- APIMatic.Core/Utilities/ICompatibilityFactory.cs
+++ APIMatic.Core/Utilities/ICompatibilityFactory.cs
@@ -5,6 +5,13 @@ using APIMatic.Core.Types.Sdk;
 
 namespace APIMatic.Core.Utilities
 {
+    /// <summary>
+    /// Provides compatibile conversion from parent to child classes
+    /// </summary>
+    /// <typeparam name="Request"> Class Type that holds http request info </typeparam>
+    /// <typeparam name="Response"> Class Type that holds http response info </typeparam>
+    /// <typeparam name="Context"> Class Type that holds http context info </typeparam>
+    /// <typeparam name="ApiException"> Class Type that holds BaseException info </typeparam>
     public interface ICompatibilityFactory<Request, Response, Context, ApiException>
         where Request : CoreRequest
         where Response : CoreResponse
