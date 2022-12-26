311e24883b821ea908f50a74a24e1e2c88e03b67
Hamza shoukat <hamza.shoukat@apimatic.io>
2022-12-26 12:16:36 +0500

    Rfc1123 DateTime fixes

--- APIMatic.Core.Test/Utilities/Date/Xml/Rfc1123DateTimeXmlUtilityTest.cs
+++ APIMatic.Core.Test/Utilities/Date/Xml/Rfc1123DateTimeXmlUtilityTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using APIMatic.Core.Utilities.Date.Xml;
 using NUnit.Framework;
 
@@ -21,8 +19,8 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void StringToRfc1123Date_WithValidString()
         {
-            DateTime expected = new DateTime(2017, 1, 18, 6, 1, 3);
             string datetimeString = "Wed, 18 Jan 2017 01:01:03 GMT";
+            var expected = StringToRfc1123DateTime(datetimeString);
             DateTime? actual = Rfc1123DateTimeXmlConverter.StringToRfc1123Date(datetimeString);
             Assert.AreEqual(expected, actual);
         }
@@ -31,7 +29,7 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         public void Rfc1123DateToString_WithValidDate()
         {
             DateTime dateTime = new DateTime(2017, 1, 18, 6, 1, 3);
-            string expected = "Wed, 18 Jan 2017 01:01:03 GMT";
+            var expected = Rfc1123DateTimeToString(dateTime);
             string actual = Rfc1123DateTimeXmlConverter.Rfc1123DateToString(dateTime);
             Assert.AreEqual(expected, actual);
         }
@@ -40,7 +38,8 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         public void ToRfc1123DateTimeXml_WithValidDate()
         {
             DateTime dateTime = new DateTime(2017, 1, 18, 6, 1, 3);
-            string expected = "<DateTime>Wed, 18 Jan 2017 01:01:03 GMT</DateTime>";
+            string expectedDateTime = Rfc1123DateTimeToString(dateTime);
+            string expected = $"<DateTime>{expectedDateTime}</DateTime>";
             string actual = Rfc1123DateTimeXmlConverter.ToRfc1123DateTimeXml(dateTime);
             Assert.AreEqual(expected, actual);
         }
@@ -48,8 +47,9 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void FromRfc1123DateTimeXml_WithValidString()
         {
-            string dateTimeString = "<DateTime>Wed, 18 Jan 2017 01:01:03 GMT</DateTime>";
-            DateTime? expected = new DateTime(2017, 1, 18, 6, 1, 3);
+            string dateTime = "Wed, 18 Jan 2017 01:01:03 GMT";
+            string dateTimeString = $"<DateTime>{dateTime}</DateTime>";
+            DateTime? expected = StringToRfc1123DateTime(dateTime);
             DateTime? actual = Rfc1123DateTimeXmlConverter.FromRfc1123DateTimeXml(dateTimeString);
             Assert.AreEqual(expected, actual);
         }
@@ -82,12 +82,14 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void ToRfc1123DateTimeXml_WithValidDateArrayNodeName()
         {
+            DateTime dateTime = new DateTime(2017, 1, 18, 6, 3, 1);
             List<DateTime> dateTimes = new List<DateTime>
             {
-                new DateTime(2017, 1, 18, 6, 3, 1),
-                new DateTime(2017, 1, 18, 6, 3, 1)
+                dateTime,
+                dateTime
             };
-            string expected = "<DateTime>\n  <dateTime>\n    <dateTime>Wed, 18 Jan 2017 01:03:01 GMT</dateTime>\n    <dateTime>Wed, 18 Jan 2017 01:03:01 GMT</dateTime>\n  </dateTime>\n</DateTime>";
+            string expectedDateTime = Rfc1123DateTimeToString(dateTime);
+            string expected = $"<DateTime>\n  <dateTime>\n    <dateTime>{expectedDateTime}</dateTime>\n    <dateTime>{expectedDateTime}</dateTime>\n  </dateTime>\n</DateTime>";
             string actual = StringReplacer.ReplaceBackSlashR(Rfc1123DateTimeXmlConverter.ToRfc1123DateTimeListXml(dateTimes, arrayNodeName: "dateTime"));
             Assert.AreEqual(expected, actual);
         }
@@ -95,12 +97,14 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void ToRfc1123DateTimeXml_WithValidDateWithoutArrayNode()
         {
+            DateTime dateTime = new DateTime(2017, 1, 18, 6, 3, 1);
             List<DateTime> dateTimes = new List<DateTime>
             {
-                new DateTime(2017, 1, 18, 6, 3, 1),
-                new DateTime(2017, 1, 18, 6, 3, 1)
+                dateTime,
+                dateTime
             };
-            string expected = "<DateTime>\n  <dateTime>Wed, 18 Jan 2017 01:03:01 GMT</dateTime>\n  <dateTime>Wed, 18 Jan 2017 01:03:01 GMT</dateTime>\n</DateTime>";
+            string expectedDateTime = Rfc1123DateTimeToString(dateTime);
+            string expected = $"<DateTime>\n  <dateTime>{expectedDateTime}</dateTime>\n  <dateTime>{expectedDateTime}</dateTime>\n</DateTime>";
             string actual = StringReplacer.ReplaceBackSlashR(Rfc1123DateTimeXmlConverter.ToRfc1123DateTimeListXml(dateTimes));
             Assert.AreEqual(expected, actual);
         }
@@ -108,10 +112,12 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void FromRfc1123DateTimeListXml_WithValidString()
         {
+            string rfc1123DateTimeString = "Wed, 18 Jan 2017 01:03:01 GMT";
+            DateTime dateTime = StringToRfc1123DateTime(rfc1123DateTimeString);
             List<DateTime> expected = new List<DateTime>
             {
-                new DateTime(2017, 1, 18, 6, 3, 1),
-                new DateTime(2017, 1, 18, 6, 3, 1)
+                dateTime,
+                dateTime
             };
             string rfc1123DateTime = "<DateTime>\r\n  <dateTime>Wed, 18 Jan 2017 01:03:01 GMT</dateTime>\r\n  <dateTime>Wed, 18 Jan 2017 01:03:01 GMT</dateTime>\r\n</DateTime>";
             List<DateTime> actual = Rfc1123DateTimeXmlConverter.FromRfc1123DateTimeListXml(rfc1123DateTime);
@@ -124,6 +130,20 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
             string rfc1123DateTime = string.Empty;
             List<DateTime> actual = Rfc1123DateTimeXmlConverter.FromRfc1123DateTimeListXml(rfc1123DateTime);
             Assert.IsNull(actual);
+        }
+
+        private static DateTime StringToRfc1123DateTime(string datetimeString)
+        {
+            DateTime expected;
+            var provider = CultureInfo.InvariantCulture;
+
+            expected = DateTime.ParseExact(datetimeString, "r", provider).ToLocalTime();
+            return expected;
+        }
+
+        private static string Rfc1123DateTimeToString(DateTime dateTime)
+        {
+            return dateTime.ToUniversalTime().ToString("r");
         }
     }
 }
