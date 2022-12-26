6836f940e15160b5e12ddfd587193fc86227c337
Hamza shoukat <hamza.shoukat@apimatic.io>
2022-12-26 12:04:27 +0500

    RFC3339 fixes to make platform independent

--- APIMatic.Core.Test/Utilities/Date/Xml/Rfc3339DateTimeXmlUtilityTest.cs
+++ APIMatic.Core.Test/Utilities/Date/Xml/Rfc3339DateTimeXmlUtilityTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
 using APIMatic.Core.Utilities.Date.Xml;
 using NUnit.Framework;
 
@@ -18,8 +19,8 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void StringToRfc3339Date_WithValidString()
         {
-            DateTime expected = new DateTime(2017, 1, 18, 6, 1, 3);
             string dateTimeString = "2017-01-18T06:01:03Z";
+            var expected = StringToRfc3339DateTime(dateTimeString);
             DateTime? actual = Rfc3339DateTimeXmlConverter.StringToRfc3339Date(dateTimeString);
             Assert.AreEqual(expected, actual);
         }
@@ -27,8 +28,8 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void Rfc3339DateToString_WithValidDate()
         {
-            DateTime dateTime = new DateTime(2017, 1, 18, 6, 1, 3);
-            string expected = "2017-01-18T06:01:03Z";
+            DateTime? dateTime = new DateTime(2017, 1, 18, 6, 1, 3);
+            var expected = Rfc3339ToString(dateTime);
             string actual = Rfc3339DateTimeXmlConverter.Rfc3339DateToString(dateTime);
             Assert.AreEqual(expected, actual);
         }
@@ -45,7 +46,8 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         public void ToRfc3339DateTimeXml_WithValidDate()
         {
             DateTime dateTime = new DateTime(2017, 1, 18, 6, 1, 3);
-            string expected = "<DateTime>2017-01-18T06:01:03Z</DateTime>";
+            var expectedDateTime = Rfc3339ToString(dateTime);
+            string expected = $"<DateTime>{expectedDateTime}</DateTime>";
             string actual = Rfc3339DateTimeXmlConverter.ToRfc3339DateTimeXml(dateTime);
             Assert.AreEqual(expected, actual);
         }
@@ -62,12 +64,12 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void FromRfc3339DateTimeXmll_WithValidString()
         {
-            DateTime expected = new DateTime(2017, 1, 18, 6, 1, 3);
-            string rfc3399Xml = "<DateTime>2017-01-18T06:01:03Z</DateTime>";
+            string rfc3339DateTime = "2017-01-18T06:01:03Z";
+            string rfc3399Xml = $"<DateTime>{rfc3339DateTime}</DateTime>";
+            DateTime expected = StringToRfc3339DateTime(rfc3339DateTime);
             DateTime? actual = Rfc3339DateTimeXmlConverter.FromRfc3339DateTimeXml(rfc3399Xml);
             Assert.AreEqual(expected, actual);
         }
-
 
         [Test]
         public void FromRfc3339DateTimeXmll_WithEmptyBlockString()
@@ -88,13 +90,14 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void ToRfc3339DateTimeXml_WithDate()
         {
+            DateTime dateTime = new DateTime(2017, 1, 18, 6, 3, 1);
             List<DateTime?> dateTimes = new List<DateTime?>
             {
-                new DateTime(2017, 1, 18, 6, 3, 1),
-                new DateTime(2017, 1, 18, 6, 3, 1)
+                dateTime,
+                dateTime
             };
-
-            string expected = "<DateTime>\n  <dateTime>2017-01-18T06:03:01Z</dateTime>\n  <dateTime>2017-01-18T06:03:01Z</dateTime>\n</DateTime>";
+            string rfcDateTimeString = Rfc3339ToString(dateTime);
+            string expected = $"<DateTime>\n  <dateTime>{rfcDateTimeString}</dateTime>\n  <dateTime>{rfcDateTimeString}</dateTime>\n</DateTime>";
             string actual = StringReplacer.ReplaceBackSlashR(Rfc3339DateTimeXmlConverter.ToRfc3339DateTimeListXml(dateTimes));
             Assert.AreEqual(expected, actual);
         }
@@ -102,13 +105,14 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void ToRfc3339DateTimeXml_WithDateArrayNodeName()
         {
+            DateTime dateTime = new DateTime(2017, 1, 18, 6, 3, 1);
             List<DateTime?> dateTimes = new List<DateTime?>
             {
-                new DateTime(2017, 1, 18, 6, 3, 1),
-                new DateTime(2017, 1, 18, 6, 3, 1)
+                dateTime,
+                dateTime
             };
-
-            string expected = "<DateTime>\n  <dateTime>\n    <dateTime>2017-01-18T06:03:01Z</dateTime>\n    <dateTime>2017-01-18T06:03:01Z</dateTime>\n  </dateTime>\n</DateTime>";
+            string rfcDateTimeString = Rfc3339ToString(dateTime);
+            string expected = $"<DateTime>\n  <dateTime>\n    <dateTime>{rfcDateTimeString}</dateTime>\n    <dateTime>{rfcDateTimeString}</dateTime>\n  </dateTime>\n</DateTime>";
             string actual = StringReplacer.ReplaceBackSlashR(Rfc3339DateTimeXmlConverter.ToRfc3339DateTimeListXml(dateTimes, arrayNodeName: "dateTime"));
             Assert.AreEqual(expected, actual);
         }
@@ -127,11 +131,13 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
         [Test]
         public void FromRfc3339DateTimeListXml_WithValidStringDate()
         {
+            string rfc3339DateTime = "2017-01-18T06:03:01Z";
+            DateTime dateTime = StringToRfc3339DateTime(rfc3339DateTime);
             string rfc3339DateTimes = "<DateTime>\r\n  <dateTime>2017-01-18T06:03:01Z</dateTime>\r\n  <dateTime>2017-01-18T06:03:01Z</dateTime>\r\n</DateTime>";
             List<DateTime> expected = new List<DateTime>
             {
-                new DateTime(2017, 1, 18, 6, 3, 1, DateTimeKind.Utc),
-                new DateTime(2017, 1, 18, 6, 3, 1, DateTimeKind.Utc)
+                dateTime,
+                dateTime
             };
             List<DateTime> actual = Rfc3339DateTimeXmlConverter.FromRfc3339DateTimeListXml(rfc3339DateTimes);
             Assert.AreEqual(expected, actual);
@@ -143,6 +149,16 @@ namespace APIMatic.Core.Test.Utilities.Date.Xml
             string rfc3339DateTimes = string.Empty;
             List<DateTime> actual = Rfc3339DateTimeXmlConverter.FromRfc3339DateTimeListXml(rfc3339DateTimes);
             Assert.IsNull(actual);
+        }
+
+        private static DateTime StringToRfc3339DateTime(string dateTimeString)
+        {
+            return XmlConvert.ToDateTime(dateTimeString, XmlDateTimeSerializationMode.Utc);
+        }
+
+        private static string Rfc3339ToString(DateTime? dateTime)
+        {
+            return XmlConvert.ToString(dateTime.GetValueOrDefault(), XmlDateTimeSerializationMode.Utc);
         }
     }
 }
