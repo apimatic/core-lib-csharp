910ae57df23aeda4109b3d397223388df3e9bf29
hamzamahmood <hamzamehmood18@gmail.com>
2023-01-25 14:39:13 +0500

    Add HttpStringResponse support added in the tester project

--- APIMatic.Core.Test/MockTypes/Utilities/CompatibilityFactory.cs
+++ APIMatic.Core.Test/MockTypes/Utilities/CompatibilityFactory.cs
@@ -22,7 +22,13 @@ namespace APIMatic.Core.Test.MockTypes.Utilities
             new(request.HttpMethod, request.QueryUrl, request.Headers, request.Body,
                 request.FormParameters, request.Username, request.Password, request.QueryParameters);
 
-        public HttpResponse CreateHttpResponse(CoreResponse response) =>
-            new(response.StatusCode, response.Headers, response.RawBody);
+        public HttpResponse CreateHttpResponse(CoreResponse response)
+        {
+            if (response.Body != null)
+            {
+                return new HttpStringResponse(response.StatusCode, response.Headers, response.RawBody, response.Body);
+            }
+            return new HttpResponse(response.StatusCode, response.Headers, response.RawBody);
+        }
     }
 }
