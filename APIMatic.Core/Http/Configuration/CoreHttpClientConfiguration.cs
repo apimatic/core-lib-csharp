948a8bef3c1ba55e6f20f6ff43f9aa7a01b0efae
M.Hamza Shahzad <hamzashahzad664343@gmail.com>
2025-05-15 17:29:34 +0500

    reverted corehttpclientconfiguration file

--- APIMatic.Core/Http/Configuration/CoreHttpClientConfiguration.cs
+++ APIMatic.Core/Http/Configuration/CoreHttpClientConfiguration.cs
@@ -5,9 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using APIMatic.Core.Proxy;
 
 namespace APIMatic.Core.Http.Configuration
 {
@@ -20,8 +18,6 @@ namespace APIMatic.Core.Http.Configuration
         /// Initializes a new instance of the <see cref="CoreHttpClientConfiguration"/>
         /// class.
         /// </summary>
-        public CoreProxyConfiguration ProxyConfiguration { get; }
-
         private CoreHttpClientConfiguration(
             TimeSpan timeout,
             bool skipSslCertVerification,
@@ -32,8 +28,7 @@ namespace APIMatic.Core.Http.Configuration
             IList<int> statusCodesToRetry,
             IList<HttpMethod> requestMethodsToRetry,
             HttpClient httpClientInstance,
-            bool overrideHttpClientConfiguration,
-            CoreProxyConfiguration proxyConfiguration)
+            bool overrideHttpClientConfiguration)
         {
             Timeout = timeout;
             SkipSslCertVerification = skipSslCertVerification;
@@ -45,20 +40,59 @@ namespace APIMatic.Core.Http.Configuration
             RequestMethodsToRetry = requestMethodsToRetry;
             HttpClientInstance = httpClientInstance;
             OverrideHttpClientConfiguration = overrideHttpClientConfiguration;
-            ProxyConfiguration = proxyConfiguration;
         }
 
+        /// <summary>
+        /// Gets Http client timeout.
+        /// </summary>
         public TimeSpan Timeout { get; }
+
+        /// <summary>
+        /// Gets Whether to skip verification of SSL certificates.
+        /// </summary>
         public bool SkipSslCertVerification { get; }
+
+        /// <summary>
+        /// Gets Number of times the request is retried.
+        /// </summary>
         public int NumberOfRetries { get; }
+
+        /// <summary>
+        /// Gets Exponential backoff factor for duration between retry calls.
+        /// </summary>
         public int BackoffFactor { get; }
+
+        /// <summary>
+        /// Gets The time interval between the endpoint calls.
+        /// </summary>
         public double RetryInterval { get; }
+
+        /// <summary>
+        /// Gets The maximum retry wait time.
+        /// </summary>
         public TimeSpan MaximumRetryWaitTime { get; }
+
+        /// <summary>
+        /// Gets List of Http status codes to invoke retry.
+        /// </summary>
         public IList<int> StatusCodesToRetry { get; }
+
+        /// <summary>
+        /// Gets List of Http request methods to invoke retry.
+        /// </summary>
         public IList<HttpMethod> RequestMethodsToRetry { get; }
+
+        /// <summary>
+        /// Gets HttpClient instance used to make the HTTP calls
+        /// </summary>
         public HttpClient HttpClientInstance { get; }
+
+        /// <summary>
+        /// Gets Boolean which allows the SDK to override http client instance's settings used for features like retries, timeouts etc.
+        /// </summary>
         public bool OverrideHttpClientConfiguration { get; }
 
+        /// <inheritdoc/>
         public override string ToString()
         {
             return "HttpClientConfiguration: " +
@@ -74,9 +108,13 @@ namespace APIMatic.Core.Http.Configuration
                 $"{OverrideHttpClientConfiguration} ";
         }
 
+        /// <summary>
+        /// Creates an object of the HttpClientConfiguration using the values provided for the builder.
+        /// </summary>
+        /// <returns>Builder.</returns>
         public Builder ToBuilder()
         {
-            var builder = new Builder(ProxyConfiguration)
+            var builder = new Builder()
                 .Timeout(Timeout)
                 .SkipSslCertVerification(SkipSslCertVerification)
                 .NumberOfRetries(NumberOfRetries)
@@ -95,7 +133,6 @@ namespace APIMatic.Core.Http.Configuration
         /// </summary>
         public class Builder
         {
-            private readonly CoreProxyConfiguration proxyConfiguration;
             private TimeSpan timeout = TimeSpan.FromSeconds(100);
             private bool skipSslCertVerification = false;
             private int numberOfRetries = 0;
@@ -112,58 +149,101 @@ namespace APIMatic.Core.Http.Configuration
             }.Select(val => new HttpMethod(val)).ToImmutableList();
             private HttpClient httpClientInstance = null;
             private bool overrideHttpClientConfiguration = true;
-            public Builder(CoreProxyConfiguration proxyConfig)
-            {
-                proxyConfiguration = proxyConfig;
-            }
 
+            /// <summary>
+            /// Sets the Timeout.
+            /// </summary>
+            /// <param name="timeout"> Timeout. </param>
+            /// <returns>Builder.</returns>
             public Builder Timeout(TimeSpan timeout)
             {
                 this.timeout = timeout.TotalSeconds <= 0 ? TimeSpan.FromSeconds(100) : timeout;
                 return this;
             }
 
+            /// <summary>
+            /// Sets the SkipSslCertVerification.
+            /// </summary>
+            /// <param name="skipSslCertVerification">Bool for skipping (or not) SSL certificate verification</param>
+            /// <returns>Builder.</returns>
             public Builder SkipSslCertVerification(bool skipSslCertVerification)
             {
                 this.skipSslCertVerification = skipSslCertVerification;
                 return this;
             }
 
+            /// <summary>
+            /// Sets the NumberOfRetries.
+            /// </summary>
+            /// <param name="numberOfRetries"> NumberOfRetries. </param>
+            /// <returns>Builder.</returns>
             public Builder NumberOfRetries(int numberOfRetries)
             {
                 this.numberOfRetries = numberOfRetries < 0 ? 0 : numberOfRetries;
                 return this;
             }
 
+            /// <summary>
+            /// Sets the BackoffFactor.
+            /// </summary>
+            /// <param name="backoffFactor"> BackoffFactor. </param>
+            /// <returns>Builder.</returns>
             public Builder BackoffFactor(int backoffFactor)
             {
                 this.backoffFactor = backoffFactor < 1 ? 2 : backoffFactor;
                 return this;
             }
 
+            /// <summary>
+            /// Sets the RetryInterval.
+            /// </summary>
+            /// <param name="retryInterval"> RetryInterval. </param>
+            /// <returns>Builder.</returns>
             public Builder RetryInterval(double retryInterval)
             {
                 this.retryInterval = retryInterval < 0 ? 1 : retryInterval;
                 return this;
             }
 
+            /// <summary>
+            /// Sets the MaximumRetryWaitTime.
+            /// </summary>
+            /// <param name="maximumRetryWaitTime"> MaximumRetryWaitTime. </param>
+            /// <returns>Builder.</returns>
             public Builder MaximumRetryWaitTime(TimeSpan maximumRetryWaitTime)
             {
                 this.maximumRetryWaitTime = maximumRetryWaitTime.TotalSeconds < 0 ? TimeSpan.FromSeconds(120) : maximumRetryWaitTime;
                 return this;
             }
 
+            /// <summary>
+            /// Sets the StatusCodesToRetry.
+            /// </summary>
+            /// <param name="statusCodesToRetry"> StatusCodesToRetry. </param>
+            /// <returns>Builder.</returns>
             public Builder StatusCodesToRetry(IList<int> statusCodesToRetry)
             {
                 this.statusCodesToRetry = statusCodesToRetry ?? new List<int>().ToImmutableList();
                 return this;
             }
 
+            /// <summary>
+            /// Sets the RequestMethodsToRetry.
+            /// </summary>
+            /// <param name="requestMethodsToRetry"> RequestMethodsToRetry. </param>
+            /// <returns>Builder.</returns>
             public Builder RequestMethodsToRetry(IList<HttpMethod> requestMethodsToRetry)
             {
                 this.requestMethodsToRetry = requestMethodsToRetry ?? new List<HttpMethod>().ToImmutableList();
                 return this;
             }
+
+            /// <summary>
+            /// Sets the HttpClientInstance.
+            /// </summary>
+            /// <param name="httpClientInstance"> HttpClientInstance. </param>
+            /// <param name="overrideHttpClientConfiguration"> OverrideHttpClientConfiguration. </param>
+            /// <returns>Builder.</returns>
             public Builder HttpClientInstance(HttpClient httpClientInstance, bool overrideHttpClientConfiguration = true)
             {
                 this.httpClientInstance = httpClientInstance;
@@ -171,6 +251,10 @@ namespace APIMatic.Core.Http.Configuration
                 return this;
             }
 
+            /// <summary>
+            /// Creates an object of the HttpClientConfiguration using the values provided for the builder.
+            /// </summary>
+            /// <returns>HttpClientConfiguration.</returns>
             public CoreHttpClientConfiguration Build()
             {
                 return new CoreHttpClientConfiguration(
@@ -182,32 +266,33 @@ namespace APIMatic.Core.Http.Configuration
                         maximumRetryWaitTime,
                         statusCodesToRetry,
                         requestMethodsToRetry,
-                        httpClientInstance ?? GetInitializedHttpClientInstance(),
-                        overrideHttpClientConfiguration,
-                        proxyConfiguration);
+                        GetInitializedHttpClientInstance(),
+                        overrideHttpClientConfiguration);
             }
 
             private HttpClient GetInitializedHttpClientInstance()
             {
-                var handler = new HttpClientHandler();
-
-                if (proxyConfiguration?.Address != null)
+                if (overrideHttpClientConfiguration)
                 {
-                    Console.WriteLine($"Applying Core Proxy: {proxyConfiguration.Address}:{proxyConfiguration.Port}");
-
-                    var proxy = new WebProxy(proxyConfiguration.Address, proxyConfiguration.Port)
+                    if (skipSslCertVerification)
                     {
-                        BypassProxyOnLocal = false,
-                        Credentials = !string.IsNullOrEmpty(proxyConfiguration.User)
-                            ? new NetworkCredential(proxyConfiguration.User, proxyConfiguration.Pass)
-                            : null
-                    };
+                        var httpClientHandler = new HttpClientHandler
+                        {
+                            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
+                        };
+                        return new HttpClient(httpClientHandler, disposeHandler: true)
+                        {
+                            Timeout = timeout,
+                        };
+                    }
 
-                    handler.Proxy = proxy;
-                    handler.UseProxy = true;
+                    var httpClient = httpClientInstance ?? new HttpClient();
+                    httpClient.Timeout = timeout;
+
+                    return httpClient;
                 }
 
-                return new HttpClient(handler);
+                return httpClientInstance ?? new HttpClient();
             }
         }
     }
